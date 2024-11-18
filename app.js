057469f426715bdefba2f1d7fe074ed83132065c
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-11-18 18:29:36 +0100

    add

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const list = require("./list.js")
+
 app.listen(port, () => {
     console.log(`server in ascolto sula porta ${port}`)
 })
 app.get("/", (req, res) => {
     res.send("server del mio bog")
 })
+
+app.get("/bacheca", (req, res) => {
+    const newList = {
+        count: list.length + 1,
+        posts: list
+
+    }
+    res.json(newList)
+})
+
---
+++ list.js
@@ -0,0 +1,35 @@
+const list =
+    [
+        {
+            title: "titolo1",
+            content: "contenuto1",
+            img: "./public/img/ciambellone.jpeg",
+            tags: ["assd", "asda", "aeqwd"]
+        },
+        {
+            title: "titolo1",
+            content: "contenuto1",
+            img: "./public/img/cracker_barbabietola.jpeg",
+            tags: ["assd", "asda", "aeqwd"]
+        },
+        {
+            title: "titolo1",
+            content: "contenuto1",
+            img: "./public/img/pane_fritto_dolce.jpeg",
+            tags: ["assd", "asda", "aeqwd"]
+        },
+        {
+            title: "titolo1",
+            content: "contenuto1",
+            img: "./public/img/pasta_barbabietola.jpeg",
+            tags: ["assd", "asda", "aeqwd"]
+        },
+        {
+            title: "titolo1",
+            content: "contenuto1",
+            img: "./public/img/torta_paesana.jpeg",
+            tags: ["assd", "asda", "aeqwd"]
+        }
+    ]
+
+module.exports = list
